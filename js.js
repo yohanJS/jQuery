77d978a1e6dd8a22e9871e621cc3b434f75b5ca4
Yohan_Garcia <75887329+yohanJS@users.noreply.github.com>
2022-12-04 20:00:13 -0500

    height added to the col navBar

--- js.js
+++ js.js
@@ -5,7 +5,7 @@ $(document).ready(()=> {
         $('.navBar').slideToggle();
         if($('#toggle').text() == 'menu') {
             $('#toggle').text('close');
-            console.log('hello');      
+                
         } else {
             $('#toggle').text('menu');
         }
